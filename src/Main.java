2476f82f54d19cad61da52a4e291f511a5c1993e
rperaltah <134903914+rperaltah@users.noreply.github.com>
2023-06-13 23:38:42 -0400

    tema 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int numeroIF = 10;
+        if (numeroIF < 0)
+            System.out.println("Negativo");
+        else if (numeroIF > 0) {
+            System.out.println("Positivo");
+        } else {
+            System.out.println("Es Cero");
+        }
+
+
+        int numeroWhile = 0;
+
+        while (numeroWhile < 3) {
+            System.out.println(numeroWhile ++);
+        }
+        do { numeroWhile++;
+            System.out.println(numeroWhile);
+        } while (numeroWhile <3 );
+
+
+        for (int numeroFor =0; numeroFor <= 3; numeroFor++ ) {
+            System.out.println(numeroFor);
+        }
+
+        String estacion = "Verano";
+
+        switch (estacion) {
+            case "Verano":
+                System.out.println("Estamos en Verano");
+                break;
+            case "Invierno":
+                System.out.println("Estamos en Invierno");
+                break;
+            case "Otoño":
+                System.out.println("Estamos en Otoño");
+                break;
+            case "Primavera":
+                System.out.println("Estamos en Primavera");
+                break;
+            default:
+                System.out.println("NO ES UNA ESTACION VALIDA");
+
+        }
+
     }
 }
